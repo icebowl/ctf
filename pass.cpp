7349a6ebb25a1685cc47b44db62d56a2e5f61a0b
cwc <icebowl1967@gmail.com>
2021-03-10 05:18:52 -0800

    AA

--- pass.cpp
+++ pass.cpp
@@ -1,28 +1,32 @@
-#include <iostream>
-//#include <conio.h>
-using namespace std;
+#include <stdio.h>
 
-int main(){
+char cipher[] = "jU5t_a_sna_3lpm13gf49_u_4_mar24c";
+char buff[32];
+	
+int main( void )
+{
+	int i;
 
-char* password = "jU5t_a_sna_31pm13gc49_u_4_m0rf41";
-char buffer[32];
-int i = 0;
+	for ( i = 0 ; i < 32 ; i++ ) {
+		buff[i] = 'F';
+	}
+		
+	for ( i = 0 ; i < 8 ; i++ ) {
+		buff[i] = cipher[i];
+	}
 
-for (i=0; i <8; i++){
-	buffer[i] = password[i];
-}
-for (;i<16;i++){
-	buffer[i] = password[23-i];
-}
-for (;i<32;i+=2){
-	buffer[i] = password[46-i];
-}
-for (i=31;i>17;i-=2){
-	buffer[i] = password[i];
-}
+	for ( i = 8 ; i < 16 ; i++ ) {
+		buff[i] = cipher[23 - i];
+	}
 
-cout << buffer;
+	for ( i = 16 ; i < 32 ; i+=2 ) {
+		buff[i] = cipher[46 - i];
+	}
 
-return 0;
+        for ( i = 31 ; i >= 17 ; i -=2 ) {
+		buff[i] = cipher[i];
+        }
+	printf( "%s\n", buff );
+
+	return 0;
 }
-+
