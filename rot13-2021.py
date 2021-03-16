ff59f05659ff9370b2729becdfe8cb21789c3ac2
tritechsc <craig.coleman@ksd.org>
2021-03-16 12:00:10 -0700

    AA

--- rot13-2021.py
+++ rot13-2021.py
@@ -7,7 +7,7 @@ rot13trans = maketrans('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
 def rot13(text):
    return text.translate(rot13trans)
 def main():
-   txt = "ROT13 Algorithm"
+   txt = "cvpbPGS{arkg_gvzr_V'yy_gel_2_ebhaqf_bs_ebg13_nSkgmDJE}"
    print (rot13(txt))
 	
 if __name__ == "__main__":
